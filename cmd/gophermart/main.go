06a715327e242b1860009b7d40ef0bc568ed45cb
Alexey Timofeev <TImofeevAV8f@yandex.ru>
2026-03-25 00:40:51 +0300

    запуск свагера

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	api "github.com/gearwheels/gophermart_cp1/internal/pkg/api"
-	openapischema "github.com/gearwheels/gophermart_cp1/api"
 	"net/http"
+
+	openapischema "github.com/gearwheels/gophermart_cp1/api"
+	api "github.com/gearwheels/gophermart_cp1/internal/pkg/api"
 
 	"github.com/labstack/echo/v4"
 )
 
-
-
 func main() {
 	server := api.NewServer()
 
-    e := echo.New()
+	e := echo.New()
 
-    // api.RegisterHandlers(e, api.NewStrictHandler(
-    //     server,
-    //     // сюда можно добавить middlewares, если нужно
-    //     []api.StrictMiddlewareFunc{},
-    // ))
+	// api.RegisterHandlers(e, api.NewStrictHandler(
+	//     server,
+	//     // сюда можно добавить middlewares, если нужно
+	//     []api.StrictMiddlewareFunc{},
+	// ))
 	api.RegisterHandlers(e, server)
 	e.GET("/swagger/*", echo.WrapHandler(http.StripPrefix("/swagger/", http.FileServer(http.FS(openapischema.SwaggerUI)))))
 
-    e.Start("127.0.0.1:8080")
+	e.Start("127.0.0.1:8080")
 }
-
-
